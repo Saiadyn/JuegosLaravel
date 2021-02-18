bc8455c2b83781e27f41851d5ebedec851b1c335
morenoide <jandromoreno3@gmail.com>
2021-02-18 15:40:34 +0100

    informacion

---
+++ resources/views/informacion.blade.php
@@ -0,0 +1,35 @@
+@extends('plantilla')
+
+@section('title', 'Informacion')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="display-1">Información sobre los juegos</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum neque vel, dolor recusandae doloribus
+                officia, natus nisi labore dicta in maxime cum repellat itaque placeat ab quia modi molestias perspiciatis!
+                
+            </div>
+        </div>
+    </div>
+@endsection
