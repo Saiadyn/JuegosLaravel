8e7de70ef5fcdb3f4c230aa79e6f0d971b5cf829
Saiadyn <santicuz13@gmail.com>
2021-02-18 15:53:14 +0100

    vx.x

---
+++ resources/views/juegos.blade.php
@@ -0,0 +1,77 @@
+@extends('plantilla')
+
+@section('title','Juegos')
+
+@section('content')
+    <h1 class="text-center">Varios Juegos</h1>
+
+<div class="row">
+    <div class="media border border-dark p-3 mt-2 col-4">
+        <img src="/imgs/j1.jpeg" class="mr-3 mt-3" style="width:50px">
+        <div class="media-body">
+          <p class="h4">Hollow Knight</p>
+          <p class="mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nemo reiciendis. Neque assumenda
+            consequatur delectus et deleniti ratione tempora molestias nemo optio beatae accusamus doloremque.
+        </p>
+        </div>
+    </div>
+
+    <div class="media border border-dark p-3 mt-2 col-4">
+        <img src="/imgs/j2.jpeg" class="mr-3 mt-3" style="width:50px">
+        <div class="media-body">
+          <p class="h4">Skyrim</p>
+          <p class="mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nemo reiciendis. Neque assumenda
+            consequatur delectus et deleniti ratione tempora molestias nemo optio beatae accusamus doloremque.
+        </p>
+        </div>
+    </div>
+
+    <div class="media border border-dark p-3 mt-2 col-4">
+        <img src="/imgs/j3.jpg" class="mr-3 mt-3" style="width:50px">
+        <div class="media-body">
+          <p class="h4">Darkest Dungeon</p>
+          <p class="mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nemo reiciendis. Neque assumenda
+            consequatur delectus et deleniti ratione tempora molestias nemo optio beatae accusamus doloremque.
+        </p>
+        </div>
+    </div>
+</div>
+
+<div class="row mb-2">
+    <div class="media border border-dark p-3 col-4">
+        <img src="/imgs/j4.jpg" class="mr-3 mt-3" style="width:50px">
+        <div class="media-body">
+          <p class="h4">Nuclear Throne</p>
+          <p class="mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nemo reiciendis. Neque assumenda
+            consequatur delectus et deleniti ratione tempora molestias nemo optio beatae accusamus doloremque.
+        </p>
+        </div>
+    </div>
+
+    <div class="media border border-dark p-3 col-4">
+        <img src="/imgs/j5.jpeg" class="mr-3 mt-3" style="width:50px">
+        <div class="media-body">
+          <p class="h4">Into The Breach</p>
+          <p class="mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nemo reiciendis. Neque assumenda
+            consequatur delectus et deleniti ratione tempora molestias nemo optio beatae accusamus doloremque.
+        </p>
+        </div>
+    </div>
+
+    <div class="media border border-dark p-3 col-4">
+        <img src="/imgs/j6.jpeg" class="mr-3 mt-3" style="width:50px">
+        <div class="media-body">
+          <p class="h4">Slay The Spire</p>
+          <p class="mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, nemo reiciendis. Neque assumenda
+            consequatur delectus et deleniti ratione tempora molestias nemo optio beatae accusamus doloremque.
+        </p>
+        </div>
+    </div>
+</div>
+@endsection
